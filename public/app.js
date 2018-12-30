cb4cc89bc36b513dbad8d5b9a9bf210175f6c935
Didi <ddapp108@gmail.com>
2018-12-30 12:37:37 -0600

    debug

--- public/app.js
+++ public/app.js
@@ -1,14 +1,14 @@
 // Grab the articles as a json
 $(document).ready(function(){
-  $.getJSON("/articles", function(data) {
-    // For each one
-    for (var i = 0; i < data.length; i++) {
-      // Display the apropos information on the page
-      $("#articles").append("<div> <img src='" + data[i].img + "'>" + "<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p><button data-id="+ data[i]._id +" class=save> Save Article </button></div>"
-      );
+  // $.getJSON("/articles", function(data) {
+  //   // For each one
+  //   for (var i = 0; i < data.length; i++) {
+  //     // Display the apropos information on the page
+  //     $("#articles").append("<div> <img src='" + data[i].img + "'>" + "<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p><button data-id="+ data[i]._id +" class=save> Save Article </button></div>"
+  //     );
 
-    }
-  });
+  //   }
+  // });
 })
 
   
