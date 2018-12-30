608ae58358ca343d5fa748e7ba6ed2f1617a43ac
Didi <ddapp108@gmail.com>
2018-12-30 12:18:07 -0600

    update

--- server.js
+++ server.js
@@ -33,7 +33,11 @@ app.use(express.static("public"));
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI, function(err) {
+    if (err) throw err;
+    console.log('Database connected')
+});
+
 
 // Routes
 
